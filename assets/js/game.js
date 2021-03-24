6c4a3a71ffcde2dd90e8b6856c82aa4e382f41de
CastroOlympias <blackcarrera@msn.com>
2021-03-24 16:58:24 -0600

    <learned some var, console strings and fuctions>

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,14 @@
-window.alert("This is an alert! javaScrpit is running!");
+// this creates a function named "fight"
+var playerName = window.prompt("What is your Robot's name?");
+// What is this?
+console.log(playerName);
+console.log("This logs a string, good for leaving yourself a message");
+// This will do math and log 20
+console.log(10 + 10);
+//What is this?
+console.log("Our Robot's name is, " + playerName);
+console.log("Your Robot, " +playerName + ", has won!");
+window.alert(playerName);
+function fight() {
+    window.alert("The fight has begun!");
+  } // fight();
